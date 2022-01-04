22e2ab5a9e71f809c15770048b49f44a4d18e8c1
石京辉 <1292077619@qq.com>
2022-01-04 09:05:57 +0800

    refactor(tsx): added viet-plugin-ts to test TSX

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueTsx from 'vite-plugin-ts'
 import { fileURLToPath } from 'url'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
-  plugins: command === 'build' ? [] : [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      packages: fileURLToPath(new URL('./packages', import.meta.url))
+    plugins: command === 'build' ? [] : [vue(), vueTsx()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            packages: fileURLToPath(new URL('./packages', import.meta.url))
+        }
+    },
+    build: {
+        target: 'es2015',
+        lib: {
+            entry: 'packages/index.ts',
+            name: 'evil-vue',
+            fileName: (format) => `evil-vue.${format}.js`,
+            formats: ['es', 'cjs']
+        },
+        rollupOptions: {
+            external: ['vue']
+        },
+        minify: false
     }
-  },
-  build: {
-    target: 'es2015',
-    lib: {
-      entry: 'packages/index.ts',
-      name: 'evil-vue',
-      fileName: (format) => `evil-vue.${format}.js`,
-      formats: ['es', 'cjs']
-    },
-    rollupOptions: {
-      external: ['vue']
-    },
-    minify: false
-  }
 }))
