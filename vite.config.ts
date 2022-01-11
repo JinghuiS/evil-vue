8e04b4c92d3496a760fbcd1bee023475cd5e3947
石京辉 <1292077619@qq.com>
2022-01-11 16:36:35 +0800

    fix(http): external axios fix http types

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ command }) => ({
             formats: ['es', 'cjs']
         },
         rollupOptions: {
-            external: ['vue', 'vue-router']
+            external: ['vue', 'vue-router', 'axios']
         },
         minify: false
     }
